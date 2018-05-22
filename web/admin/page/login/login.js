3652ca92aa44bcf781708e45900e057035f81d99
lihaomin <howie@139.com>
2018-05-22 20:19:35 +0800

    lihaomin

---
+++ web/admin/page/login/login.js
@@ -0,0 +1,32 @@
+layui.config({
+	base : "js/"
+}).use(['form','layer','jquery'],function(){
+	var form = layui.form(),
+		layer = parent.layer === undefined ? layui.layer : parent.layer,
+		$ = layui.jquery;
+	//video背景
+	$(window).resize(function(){
+		/*if($(".video-player").width() > $(window).width()){
+			$(".video-player").css({"height":$(window).height(),"width":"auto","left":-($(".video-player").width()-$(window).width())/2});
+		}else{
+			$(".video-player").css({"width":$(window).width(),"height":"auto","left":-($(".video-player").width()-$(window).width())/2});
+		}*/
+		$(".video-player").css({"width":$(document).width(),"height":$(document).height()});
+	}).resize();
+	
+	//登录按钮事件
+	form.on("submit(login)",function(data){
+		var username = data.field.username;
+		var password = data.field.password;
+		var index = layer.load(1);
+		$.get('http://localhost:8080/user/login',data.field,function(data){
+			layer.close(index);
+			if(data.success){
+				window.location.href = "../../index.html";
+			}else{
+				layer.msg(data.errorMsg);
+			}
+		});		
+		return false;
+	})
+})
---
+++ web/admin/page/userManage/list.js
@@ -0,0 +1,133 @@
+layui.config({
+	base : "js/"
+}).use(['form','layer','jquery','laypage'],function(){
+	var form = layui.form(),
+		layer = parent.layer === undefined ? layui.layer : parent.layer,
+		laypage = layui.laypage,
+		$ = layui.jquery;
+	
+	var app = new Vue({
+		el:'#app',
+		data:{
+			dataList:null,
+			pageTotal:0
+		}
+	});
+	$.get('http://localhost:8080/user/list',function(data){
+		if(data.success){
+			app.dataList = data.data;
+			app.pageTotal = data.total;
+			laypage({
+				cont: 'page',
+				pages: app.pageTotal,
+				jump: function(obj){
+					$.get('http://localhost:8080/user/list',{page:obj.curr},function(data){
+						if(data.success){
+							app.dataList = data.data;
+							app.pageTotal = data.total;
+						}
+					});
+				}
+			});
+		}
+	});
+	
+
+
+	//添加文章
+	//改变窗口大小时，重置弹窗的高度，防止超出可视区域（如F12调出debug的操作）
+	$(window).one("resize",function(){
+		$(".add_btn").click(function(){
+			var index = layui.layer.open({
+				title : "添加",
+				type : 2,
+				content : "newsAdd.html",
+				success : function(layero, index){
+					setTimeout(function(){
+						layui.layer.tips('点击此处返回列表', '.layui-layer-setwin .layui-layer-close', {
+							tips: 3
+						});
+					},500)
+				}
+			})			
+			layui.layer.full(index);
+		})
+	}).resize();
+
+	//批量删除
+	$(".batchDel").click(function(){
+		var $checkbox = $('.layui-table tbody input[type="checkbox"][name="checked"]');
+		var $checked = $('.layui-table tbody input[type="checkbox"][name="checked"]:checked');
+		if($checkbox.is(":checked")){
+			layer.confirm('确定删除选中的信息？',{icon:3, title:'提示信息'},function(index){
+				var index = layer.msg('删除中，请稍候',{icon: 16,time:false,shade:0.8});
+	            setTimeout(function(){
+	            	//删除数据
+	            	for(var j=0;j<$checked.length;j++){
+	            		for(var i=0;i<newsData.length;i++){
+							if(newsData[i].newsId == $checked.eq(j).parents("tr").find(".news_del").attr("data-id")){
+								newsData.splice(i,1);
+								newsList(newsData);
+							}
+						}
+	            	}
+	            	$('.layui-table thead input[type="checkbox"]').prop("checked",false);
+	            	form.render();
+	                layer.close(index);
+					layer.msg("删除成功");
+	            },2000);
+	        })
+		}else{
+			layer.msg("请选择需要删除的记录");
+		}
+	})
+
+	//全选
+	form.on('checkbox(allChoose)', function(data){
+		var child = $(data.elem).parents('table').find('tbody input[type="checkbox"]:not([name="show"])');
+		child.each(function(index, item){
+			item.checked = data.elem.checked;
+		});
+		form.render('checkbox');
+	});
+
+	//通过判断是否全部选中来确定全选按钮是否选中
+	form.on("checkbox(choose)",function(data){
+		var child = $(data.elem).parents('table').find('tbody input[type="checkbox"]:not([name="show"])');
+		var childChecked = $(data.elem).parents('table').find('tbody input[type="checkbox"]:not([name="show"]):checked')
+		if(childChecked.length == child.length){
+			$(data.elem).parents('table').find('thead input#allChoose').get(0).checked = true;
+		}else{
+			$(data.elem).parents('table').find('thead input#allChoose').get(0).checked = false;
+		}
+		form.render('checkbox');
+	})
+
+	//是否展示
+	form.on('switch(isShow)', function(data){
+		var index = layer.msg('修改中，请稍候',{icon: 16,time:false,shade:0.8});
+        setTimeout(function(){
+            layer.close(index);
+			layer.msg("展示状态修改成功！");
+        },2000);
+	})
+ 
+	//操作
+	$("body").on("click",".news_edit",function(){  //编辑
+		layer.alert('您点击了文章编辑按钮，由于是纯静态页面，所以暂时不存在编辑内容，后期会添加，敬请谅解。。。',{icon:6, title:'文章编辑'});
+	})
+
+	$("body").on("click",".news_del",function(){  //删除
+		var _this = $(this);
+		layer.confirm('确定删除此信息？',{icon:3, title:'提示信息'},function(index){
+			//_this.parents("tr").remove();
+			for(var i=0;i<newsData.length;i++){
+				if(newsData[i].newsId == _this.attr("data-id")){
+					newsData.splice(i,1);
+					newsList(newsData);
+				}
+			}
+			layer.close(index);
+		});
+	})
+})
